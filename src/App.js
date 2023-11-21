1b6b5d6db28873be5a052c3f176b8a2df0b1ff07
Isha-Gaonkar <142926830+Isha-Gaonkar@users.noreply.github.com>
2023-11-21 21:12:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Calculator.css";
+import Calculator from "./calculator";
+
+function App() {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
